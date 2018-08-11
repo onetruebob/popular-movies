0da9b5f365af101dd926bd3553b755d6f770a847
Bob Owen <bob@bobowen.org>
2018-08-11 11:34:59 -0400

    Add React to the project and modify the test to use compiled React from jsx.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import stringUpper from './upper';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const app = document.getElementById('app');
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { name: 'React' };
+    }
+    handleChange(e) {
+        this.setState({ name: e.target.value });
+    }
+    render() {
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>Welcome</h1>
+                <p>Hello {stringUpper(this.state.name)}</p>
+                <input onChange={this.handleChange} defaultValue={this.state.name} />
+            </div>
+        );
+    }
+}
 
-app.innerHTML = `Hello ${stringUpper('webpack')}!`;
+ReactDOM.render(<Welcome />, document.getElementById('app'));
