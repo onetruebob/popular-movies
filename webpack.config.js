2ee8081f4c81b6716c207a9c4f5a1686389b6f31
Bob Owen <bob@bobowen.org>
2018-08-11 11:11:32 -0400

    Add babel to webpack build pipeline for processing jsx

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
     }
 };
